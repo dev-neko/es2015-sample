cbb05f8710710490f509034fa7343e86d123df02
dev-neko <pi3kp85@gmail.com>
2016-04-21 22:25:57 +0900

    add main.scss. mv index.pug

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/styles')));
+
 app.get('/', function(request, response) {
   console.log('Hello world!!');
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000);
