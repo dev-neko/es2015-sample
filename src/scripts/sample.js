f12b5357956a8abf6c401bf05470aa553addb15d
dev-neko <pi3kp85@gmail.com>
2016-05-05 18:37:42 +0900

    Add test code by using 'Ava'

--- src/scripts/sample.js
+++ src/scripts/sample.js
@@ -1,19 +1,8 @@
-const evens = [0, 2, 4, 6, 8, 10];
-const odds = evens.map(v => v + 1);
-const powers= evens.map(v => v * v);
-
-console.log(`evens  :${evens}`);
-console.log(`odds   :${odds}`);
-console.log(`powers :${powers}`);
-
-function getUsers(first, last, age = -1) {
+export function getUser(first, last, age = -1) {
   const fullName = `${first} ${last}`;
   return { first, last, fullName, age};
 }
 
-const user1= getUsers('gokuu', 'son');
-console.log(user1);
-
-const user2= getUsers('yu-suke', 'urameshi', 14);
-console.log(user2);
-
+export function powers(nums) {
+  return nums.map(v => v * v);
+}
---
+++ test/test-sample.js
@@ -0,0 +1,21 @@
+import test from 'ava';
+import 'babel-core/register';
+import {getUser} from '../src/scripts/sample.js';
+
+
+test('getUsers', t => {
+  const first = 'Hanamichi';
+  const last  = 'Sakuragi';
+  const fullName = `${first} ${last}`;
+  const age   = 16;
+  const user1 = getUser(first, last, age);
+
+  t.deepEqual(user1, {first, last, fullName, age});
+});
+
+test('getOdds', t => {
+  const evens = [0, 2, 4, 6, 8];
+  const odds  = [1, 3, 5, 7, 9];
+
+  t.deepEqual(evens.map(v => v + 1), odds);
+});
